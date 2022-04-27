5cf53b108926792621421aef782b7da205cd902b
Macman17 <60798387+Macman17@users.noreply.github.com>
2022-04-27 05:59:59 -0700

    Update mock_data.py
    change category numbers

--- mock_data.py
+++ mock_data.py
@@ -9,21 +9,21 @@ mock_catalog = [
     {
         "_id": "65464",
         "title": "Cookies Sanwich",
-        "category": "1",
+        "category": "2",
         "unitPrice": 5.32,
         "image": "pic2.jpg"
     },
     {
         "_id": "54343",
         "title": "Macroons",
-        "category": "1",
+        "category": "3",
         "unitPrice": 7.00,
         "image": "pic3.jpg"
     },
     {
         "_id": "35435",
         "title": "Fruit Cookies",
-        "category": "1",
+        "category": "4",
         "unitPrice": 5.01,
         "image": "pic4.jpg"
     }
