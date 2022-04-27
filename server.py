dd9a192e90c196719de18645e2eb1905fbeea370
Macman17 <60798387+Macman17@users.noreply.github.com>
2022-04-26 19:50:53 -0700

    Update server.py
    Updated server.py

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 
+
+from turtle import title
+from unicodedata import category
+from unittest import result
 from flask import Flask
 import json
 from mock_data import mock_catalog
@@ -44,6 +48,49 @@ def get_total():
     for prod in mock_catalog:
         total += prod["unitPrice"]
 
-    return json.dumps(total)    
-# start the server
+    return json.dumps(total) 
+
+@app.route("/api/products/<id>")
+def find_product(id):
+    for prod in mock_catalog:
+        if id == prod["_id"]:
+            return json.dumps(prod) 
+
+
+@app.route("/api/products/category")
+def get_catagories():
+    catagories = []
+
+    for prod in mock_catalog:
+        cat = prod["category"]
+        if not cat in catagories:
+            catagories.append(cat)
+
+    return json.dumps(catagories)
+
+@app.route("/api/products/category/<cat_name>")
+
+def get_title(cat_name):
+    results = []
+
+    for prod in mock_catalog:
+        if prod["category"].lower() == cat_name.lower() :
+            results.append(prod)
+
+    return json.dumps(results)    
+
+@app.route("/api/products/search/<text>")
+def search_by_text(text):
+    results = []
+    text = text.lower()
+    # search and add
+    for prod in mock_catalog:
+        title = prod["title"].lower()
+        if  text in title:
+            results.append(prod)        
+
+    return json.dumps(results)
+
+
+
 app.run(debug=True)
